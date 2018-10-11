1b50e36fc2e5f3781b32caaeb378e41db8f49843
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-11 21:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name){
+  kittens.concat(name)
+}
