8133c69b14fbc1cce217b8fb3cee0330b00ffba8
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-11 21:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var kittens = [] //define your array here
 
 // Add your functions and code here
 function kittens(){
-  kittens.concat(["Milo", "Otis", "Garfield"])
+  //kittens.concat(["Milo", "Otis", "Garfield"])
+  var kittens = ["Milo", "Otis", "Garfield"];
   return kittens
 }
